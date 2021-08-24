22d4cdd9d8d0b8d9e763921b0cd56e377878c20b
Pavel <49796077+QlaVs@users.noreply.github.com>
2021-08-24 18:03:01 +0300

    Update settings.py

--- mapping_v1/settings.py
+++ mapping_v1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*jq+#_wb9#ki60v69%x&11mj369gu_*2-=4f87glams2r(s71a'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
